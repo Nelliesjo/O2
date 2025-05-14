42bc681f77283eacce1b609e22007797667c82fa
nelliesjo <nellies@metropolia.fi>
2025-05-14 13:38:18 +0300

    t

--- JS + HTML/3/T3.js
+++ JS + HTML/3/T3.js
@@ -8,7 +8,6 @@ const sum = nr1 + nr2 + nr3;
 const prd = nr1 * nr2 * nr3;
 const avr = sum / 3;
 
-
 document.querySelector('#Sum').innerHTML = 'The sum of the numbers you entered is: ' + sum + '.';
 document.querySelector('#Prd').innerHTML = 'The sum of the multiplication of the numbers you entered is: ' + prd + '.';
 document.querySelector('#Avr').innerHTML = 'The sum of the average of the numbers you entered is: ' + avr + '.';
