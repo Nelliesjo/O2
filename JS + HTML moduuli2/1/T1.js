dbd0b8706e3014402feab9d6d6eca15b9c68d520
nelliesjo <nellies@metropolia.fi>
2025-05-16 18:30:20 +0300

    t

---
+++ JS + HTML moduuli2/1/T1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const nr = [];
+
+let rep = 0
+
+while (rep<5){
+  const number = parseInt(prompt("Which number?"))
+  nr.push(number)
+  rep ++
+}
+
+for (let i = 4; i >= 0; i--) {
+        console.log(`number ${i+1}: ${nr[i]}`);
+    }
